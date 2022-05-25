52d69e0fb596cbd50b5dc0e785a7fa35cdf2e7ed
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-24 20:45:27 -0500

    Completed python algo

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,14 @@
+from pyparsing import RecursiveGrammarException
+
+
 def fibonacci(n):
-  pass
+  a = 0
+  b = 1
+  sum = 0
 
+  for i in range(1, n):
+    sum = a + b
+    a = b
+    b = sum
 
+  return sum
